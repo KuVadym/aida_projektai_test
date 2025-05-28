ba93f72b1217e7da43ace747113e7f2493f154ad
Vadym Kuzyk <kuzik94vadim@gmail.com>
2025-05-28 11:16:29 +0300

    Initial commit: added Django event app

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SMTP_SERVER = 'smtp.gmail.com' #os.getenv("SMTP_SERVER")
-SMTP_PORT = 587 #os.getenv("SMTP_PORT")
-SENDER_EMAIL = 'kuzik94vadim@gmail.com'#os.getenv("EMAIL_USER")
-SENDER_PASSWORD = 'iena wtch cacl lbod'#os.getenv("EMAIL_PASSWORD")
+SMTP_SERVER = 
+SMTP_PORT = 
+SENDER_EMAIL = 
+SENDER_PASSWORD = 
